2099209958a170a2b9e374e28b99b4f85b054611
ElenaLGM7 <168128552+ElenaLGM7@users.noreply.github.com>
2025-07-28 17:49:30 +0200

    Update utils.py

--- MindLog Backend/utils.py
+++ MindLog Backend/utils.py
@@ -1,1 +1,12 @@
+from sqlalchemy.orm import Session
+import models, schemas
 
+def create_entry(db: Session, entry: schemas.EntryCreate):
+    db_entry = models.Entry(title=entry.title, content=entry.content)
+    db.add(db_entry)
+    db.commit()
+    db.refresh(db_entry)
+    return db_entry
+
+def get_entries(db: Session):
+    return db.query(models.Entry).order_by(models.Entry.created_at.desc()).all()
